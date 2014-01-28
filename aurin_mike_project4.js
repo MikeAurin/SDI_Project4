01560dc8934593f65ebb4242f878b8257dacd56b
Mike Aurin <mikeaurin@gmail.com>
2014-01-28 18:18:14 -0500

    first Project 4 commit

---
+++ aurin_mike_project4.js
@@ -0,0 +1,17 @@
+// Mike Aurin
+// SDI 1401 Project 4
+// All of the code is in the same order as the problems on the assignment PDF
+
+// Question 1
+// Does a number follow the same pattern as 123-456-7890
+
+var myTelephone = "407-400-4321";
+var myOtherNumber = "123-456-7890";
+
+if (myOtherNumber.length === myTelephone.length) {
+	console.log(true); 
+	} else {
+		console.log(false)
+};
+	
+	
