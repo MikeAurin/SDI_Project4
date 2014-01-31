a59f428c91e90c2ba4d788e4a3128b84eb09e2bc
Mike Aurin <mikeaurin@gmail.com>
2014-01-30 22:31:18 -0500

    finsished coding the 12 problems...working on flowcharts.

--- aurin_mike_project4.js
+++ aurin_mike_project4.js
@@ -108,13 +108,73 @@ console.log("Question 9: " + myValue);
 // Question 10
 // Find a value in an array greater than a given number
 
+var numbersArray = [1, 3, 5, 7, 9]; 
+var comparisonNumber = 7; 
+	for (var i = 0; i < numbersArray.length; i++) 
+		if (numbersArray[i] > comparisonNumber) { 
+				comparisonNumber = numbersArray[i]; 
 
+console.log("Question 10: " + comparisonNumber)
+};
 
 // Question 11
 // Find the total value of an array even if some values aren't numbers
+var myArray = ["phone",8,"couch",2,"dog",10,"computer"];
 
-//myArray[3, "phone", 6, "12", "couch", 20];
+var theSum = 0;
+for (var i=0; i<myArray.length; i++) {
+  if (!isNaN(parseInt(myArray[i]))) { theSum += myArray[i]; }
+}
+console.log("Question 11: " + theSum);
 
 
 // Question 12
 // Given an array and a given key, return the sorted array.
+
+function specialSort(arrayToSort, keyOrder) {
+    arrayToSort = arrayToSort.sort(function (a, b) {
+        for (var key in keyOrder) {
+            if (!keyOrder.hasOwnProperty(key)) {
+                continue;
+            }
+            var aKey = keyOrder[key];
+            if (typeof a[aKey] === "undefined" && typeof b[aKey] === "undefined") {
+                continue;
+            }
+            if (typeof a[aKey] !== "undefined" && typeof b[aKey] === "undefined") {
+                return -1;
+            }
+            if (typeof a[aKey] === "undefined" && typeof b[aKey] !== "undefined") {
+                return 1;
+            }
+            if (a[aKey] > b[aKey]) {
+                return 1;
+            }
+            else if (a[aKey] < b[aKey]) {
+                return -1;
+            }
+        }
+        return 0;
+    });
+    return arrayToSort;
+}
+var arrayToSort = [
+    {a:2},
+    {b:2},
+    {a:1},
+    {a:3},
+    {b:3},
+    {c:3},
+    {c:2},
+    {b:1}
+];
+var keyOrder = ["a", "b", "c"];
+var sortedArray = specialSort(arrayToSort, keyOrder);
+console.log("Question 12: " + JSON.stringify(sortedArray));
+
+// Number twelve was the hardest one for me to understand based on research.  Everywhere I read
+// said that this a peculiar request.  If I understand correctly, the point of this exercise
+// is to build our library to accomplish tasks in the future, I didn't alter anything in this
+// code as I am not trying to pass this off as my own work.  I found this thanks to 
+// http://stackoverflow.com/questions/19555538/sort-an-array-of-objects-in-javascript-by-key-value
+// and it solves the problem.
